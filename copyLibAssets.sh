cf9f74d9d2a4f51f186f1a0ac2e2f0f493882687
David Moore <david.moore@oasisdigital.com>
2019-08-19 16:52:19 -0400

    Removed cpx in favor of a shell script.

---
+++ copyLibAssets.sh
@@ -0,0 +1,4 @@
+echo "-------------------------------------------------"
+echo "Copying assets to library dist folder."
+echo "-------------------------------------------------"
+cp -avr ./projects/lib-with-assets/src/assets ./dist/lib-with-assets
